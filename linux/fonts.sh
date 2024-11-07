e1c3e76bdac22c011c2b0780ebf6febbcdabd173
Chen Jiacheng <3559084904@qq.com>
2024-11-07 23:57:38 +0000

    Add KaTeX fonts.

--- linux/fonts.sh
+++ linux/fonts.sh
@@ -1,3 +1,4 @@
+yay -S --noconfirm --needed katex
 curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/all-the-icons.ttf -o fonts/all-the-icons.ttf
 curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/file-icons.ttf -o fonts/file-icons.ttf
 curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/fontawesome.ttf -o fonts/fontawesome.ttf
@@ -5,4 +6,6 @@ curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/ma
 curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/octicons.ttf -o fonts/octicons.ttf
 curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/weathericons.ttf -o fonts/weathericons.ttf
 cp -r ./fonts ~/.local/share/fonts
+mkdir ~/.local/share/fonts/KaTeX/
+cp -r ./katex/fonts/* ~/.local/share/fonts/KaTeX/
 fc-cache -fv
