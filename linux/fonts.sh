6e4301cde87a0462bac447542b9cb17a18b7bec5
Chen Jiacheng <3559084904@qq.com>
2024-11-07 22:43:48 +0000

    drop fonts repo

--- linux/fonts.sh
+++ linux/fonts.sh
@@ -1,2 +1,8 @@
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/all-the-icons.ttf -o fonts/all-the-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/file-icons.ttf -o fonts/file-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/fontawesome.ttf -o fonts/fontawesome.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/material-design-icons.ttf -o fonts/material-design-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/octicons.ttf -o fonts/octicons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/weathericons.ttf -o fonts/weathericons.ttf
 cp -r ./fonts ~/.local/share/fonts
 fc-cache -fv
