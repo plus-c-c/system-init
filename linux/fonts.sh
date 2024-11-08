c91c43d5c9400356d30da61220ce00015800aad5
Chen Jiacheng <3559084904@qq.com>
2024-11-08 00:05:41 +0000

    Optimize dir.

--- linux/fonts.sh
+++ linux/fonts.sh
@@ -1,11 +1,12 @@
-yay -S --noconfirm --needed katex
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/all-the-icons.ttf -o fonts/all-the-icons.ttf
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/file-icons.ttf -o fonts/file-icons.ttf
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/fontawesome.ttf -o fonts/fontawesome.ttf
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/material-design-icons.ttf -o fonts/material-design-icons.ttf
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/octicons.ttf -o fonts/octicons.ttf
-curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/weathericons.ttf -o fonts/weathericons.ttf
-cp -r ./fonts ~/.local/share/fonts
+mkdir all-the-icons
+mkdir ~/.local/share/fonts/all-the-icons/
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/all-the-icons.ttf -o all-the-icons/all-the-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/file-icons.ttf -o all-the-icons/file-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/fontawesome.ttf -o all-the-icons/fontawesome.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/material-design-icons.ttf -o all-the-icons/material-design-icons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/octicons.ttf -o all-the-icons/octicons.ttf
+curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/weathericons.ttf -o all-the-icons/weathericons.ttf
+cp -r ./all-the-icons/* ~/.local/share/all-the-icons/
 mkdir ~/.local/share/fonts/KaTeX/
 cp -r ./katex/fonts/* ~/.local/share/fonts/KaTeX/
 fc-cache -fv
