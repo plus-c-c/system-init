148b35b527861c80215d296c5e6da4ae2756ba3f
Chen Jiacheng <3559084904@qq.com>
2024-11-09 06:13:42 +0000

    Add picture-repo

--- ketcher/install.sh
+++
@@ -1,7 +0,0 @@
-cd Indigo
-mkdir build
-cd build
-yay -S --noconfirm --needed ninja docker maven emsdk oracle-instantclient-basic
-cmake .. -DBUILD_INDIGO=ON -DBUILD_INDIGO_WRAPPERS=ON -DBUILD_INDIGO_UTILS=ON
-cmake --build . --config Release --target indigo-java indigo-python indigo-utils-linux-x86_64 bingo-oracle
-sudo make install
