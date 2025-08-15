a122fcd827474c16c2739430c87128222ea850e6
Chen Jiacheng <3559084904@qq.com>
2025-08-15 14:36:43 +0000

    update

--- linux/lang.sh
+++ linux/lang.sh
@@ -1,14 +1,20 @@
+yay -S --noconfirm --needed emacs
 yay -S --noconfirm --needed tree-sitter-markdown
+
 yay -S --noconfirm --needed vscode-json-languageserver
+
 yay -S --noconfirm --needed racket
+raco pkg install racket-langserver
+
 yay -S --noconfirm --needed bash-language-server
+
 pip install python-lsp-bridge --break-system-packages
 
 sudo npm install -g tsx typescript typescript-language-server @prisma/language-server
 
-yay -S --noconfirm --needed mariadb
-sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-sudo systemctl enable mariadb.service
-sudo systemctl start mariadb.service
-sudo chown -R mysql:mysql /var/lib/mysql
-sudo chmod -R +777 /var/lib/mysql
+yay --noconfirm --needed -S texlive texlive-lang texlab
+
+yay --noconfirm --needed -S fcitx5-im fcitx5-chinese-addons gnome-shell-extension-kimpanel-git
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/IMModule':<'fcitx'>}"
+
+yay --noconfirm --needed -S postgresql
--- linux/proxy.sh
+++ linux/proxy.sh
@@ -3,3 +3,5 @@ sudo npm -g config set https-proxy http://127.0.0.1:7890
 
 git config --global http.proxy "http://127.0.0.1:7890"
 git config --global https.proxy "http://127.0.0.1:7890"
+
+yay --noconfirm --needed -S clash-verge-rev
