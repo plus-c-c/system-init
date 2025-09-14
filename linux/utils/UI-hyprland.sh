77f994bad3b846963ab08164395900c4977a5070
Chen Jiacheng <3559084904@qq.com>
2025-09-14 22:58:45 +0800

    Customize Hyprland.

--- linux/utils/UI-hyprland.sh
+++ linux/utils/UI-hyprland.sh
@@ -6,4 +6,9 @@ yay -S --noconfirm --needed hyprland
 cp ./UI/hyprland.conf ~/.config/hypr
 
 ###basic software
-yay -S --noconfirm --needed kitty dolphin wofi
+yay -S --noconfirm --needed kitty dolphin tofi
+
+mkdir ~/.config/tofi
+mkdir ~/.config/waybar
+cp ./UI/tofi/config ~/.config/tofi
+cp ./UI/waybar/* ~/.config/waybar
