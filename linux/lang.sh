bf40199ccdb288ad82c578b2e1f9bf30c2da2265
Chen Jiacheng <3559084904@qq.com>
2025-09-14 23:03:40 +0800

    Add Fira Mono.

--- linux/lang.sh
+++ linux/lang.sh
@@ -39,5 +39,5 @@ curl https://raw.githubusercontent.com/domtronn/all-the-icons.el/master/fonts/we
 cp -r ./all-the-icons/* ~/.local/share/all-the-icons/
 mkdir ~/.local/share/fonts/KaTeX/
 cp -r ./katex/fonts/* ~/.local/share/fonts/KaTeX/
-yay -S --noconfirm --needed wqy-microhei ttf-sourcecodepro-nerd ttf-cascadia-code-nerd
+yay -S --noconfirm --needed wqy-microhei ttf-sourcecodepro-nerd ttf-cascadia-code-nerd otf-fira-mono
 fc-cache -fv
