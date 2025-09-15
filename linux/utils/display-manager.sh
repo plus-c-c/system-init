091de9716e0e2051c2d3c546c28756e4ba6063aa
Chen Jiacheng <3559084904@qq.com>
2025-09-15 22:27:48 +0800

    Change sddm background.

--- linux/utils/display-manager.sh
+++ linux/utils/display-manager.sh
@@ -1,4 +1,5 @@
 yay -S --noconfirm --needed sddm sddm-sugar-dark
-sudo cp ./UI/sddm.conf /etc/
+sudo cp ./UI/sddm/sddm.conf /etc/
+sudo cp -f ./UI/sddm/Background.jpg /usr/share/sddm/themes/sugar-dark/
 sudo systemctl enable sddm
 sudo systemctl start sddm
