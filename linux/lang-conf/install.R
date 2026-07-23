a7f1c2af7e85c7916f7e67d02cb6dabc8be4775c
Chen Jiacheng <3559084904@qq.com>
2026-07-23 23:44:09 +0800

    feat: language configs - R, Racket, Python, TypeScript, Bash, Java

---
+++ linux/lang-conf/install.R
@@ -0,0 +1,9 @@
+install.packages("languageserver",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("openxlsx2",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("pheatmap",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("png",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("ggplot2",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("dplyr",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+
+install.packages("permute",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
+install.packages("vegan",repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN")
